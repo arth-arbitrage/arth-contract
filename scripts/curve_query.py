9b964eee896dfbdbc8969b0a9b17aca33f5995e7
rampenke <rampenke@gmail.com>
2021-03-17 21:26:29 -0700

    scripts added

--- scripts/curve_query.py
+++ scripts/curve_query.py
@@ -1,6 +1,6 @@
 import sys
 from brownie import *
-
+import time
 
 active_network = network.show_active()
 
@@ -20,8 +20,21 @@ curve_addressprovider=AddressProvider.at("0x0000000022D53366457F9d5E68Ec105046FC
 curve_registry_addr = curve_addressprovider.get_registry()
 curve_poolinfo_addr = curve_addressprovider.get_address(1)
 
+registry = Registry.at(curve_registry_addr)
+poolinfo = PoolInfo.at(curve_poolinfo_addr)
 
+def showPool(i):
+    pool_addr = registry.pool_list(i)
+    pinfo = poolinfo.get_pool_info(pool_addr)
+    #token0 = interface.IERC20(pair.token0())
+    #token1 = interface.IERC20(pair.token1())
+    # print(f"pair {pair.name()}, {pair.symbol()}, {pair.token0()}, {pair.token1()} {reserve0/1E18} {reserve1/1E18} {blockTimestampLast}")
+    print(pinfo)
 
+def showPools(i):
+    for j in range (i):
+        showPool(j)
+    time.sleep(1)
+    
 def main():
-    accounts.load("account1")    
-    ArthCurveSwap.deploy(curve_registry_addr, {'from':accounts[0]})
+    pass
